45cb3c452eb434d9e221d7cc0794ac08723d1fa2
pratikjoil999 <pratikjoil999@gmail.com>
2022-05-09 19:35:58 +0530

    latest

--- app.js
+++ app.js
@@ -1,37 +1,34 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const path = require('path')
-const server = require('http').createServer(app)
-var employeeRouter = require('./routers/v1/employee/employee');
-var departmentRouter = require('./routers/v1/department/department')
-var tweetsRouter = require('./routers/v1/tweets/tweets');
-var twitterUserRouter = require('./routers/v1/twitter_user/twitter_user')
-var mongoConfig = require('./config/dbconfig');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+const server = require("http").createServer(app);
+var tweetsRouter = require("./routers/v1/tweets/tweets");
+var twitterUserRouter = require("./routers/v1/twitter_user/twitter_user");
+var mongoConfig = require("./config/dbconfig");
 
-app.use(express.json({limit : '5mb', extended : true}))
-app.use(express.urlencoded({limit:'5mb' , extended : true , parameterLimit : 50000}))
-app.use(cors())
+app.use(express.json({ limit: "5mb", extended: true }));
+app.use(
+  express.urlencoded({ limit: "5mb", extended: true, parameterLimit: 50000 })
+);
+app.use(cors());
 
 var port = process.env.PORT || 10010;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => {
-    res.render('index');
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.use('/api/v1/employee', employeeRouter);
-app.use('/api/v1/department', departmentRouter);
-app.use('/api/v1/tweets', tweetsRouter);
-app.use('/api/v1/twitter_user', twitterUserRouter);
-
+app.use("/api/v1/tweets", tweetsRouter);
+app.use("/api/v1/twitter_user", twitterUserRouter);
 
 app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
-})
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
 server.listen(port);
-console.log('App running on :>> ', port);
+console.log("App running on :>> ", port);
 module.exports = app;
